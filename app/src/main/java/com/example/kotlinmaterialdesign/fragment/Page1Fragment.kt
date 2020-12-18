66239579e692b7b1a910a10e92c1f9031b69e843
lk <18513075404@163.com>
2020-12-18 17:58:30 +0800

    menu tipmessage

--- app/src/main/java/com/example/kotlinmaterialdesign/fragment/Page1Fragment.kt
+++ app/src/main/java/com/example/kotlinmaterialdesign/fragment/Page1Fragment.kt
@@ -1,6 +1,21 @@
 package com.example.kotlinmaterialdesign.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.kotlinmaterialdesign.R
 
-class Page1Fragment: Fragment() {
+
+class Page1Fragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_page1, container, false)
+    }
+
 }
