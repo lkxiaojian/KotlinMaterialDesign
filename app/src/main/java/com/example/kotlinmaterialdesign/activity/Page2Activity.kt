6b2c8f38cf6a16f8494cedbc5e218ac5e1161a80
lk <18513075404@163.com>
2021-01-05 15:54:00 +0800

    CollapsingToolbarLayout

---
+++ app/src/main/java/com/example/kotlinmaterialdesign/activity/Page2Activity.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinmaterialdesign.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Page2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_page2)
+    }
+}
