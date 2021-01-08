0642ec29a1efe8072d6012c1ea5a9a32ebdb92a4
lk <18513075404@163.com>
2021-01-08 15:39:21 +0800

    menu

--- app/src/main/java/com/example/kotlinmaterialdesign/MainActivity.kt
+++ app/src/main/java/com/example/kotlinmaterialdesign/MainActivity.kt
@@ -10,6 +10,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import com.example.kotlinmaterialdesign.activity.Page2Activity
@@ -74,6 +75,7 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, it.title, Toast.LENGTH_SHORT).show()
             true
         }
+
 
 
         navigationView.setNavigationItemSelectedListener {
